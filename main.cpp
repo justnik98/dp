d8e763ca58e05fd307841a49f9c4d7b7d14a58a5
Nikita Yankovskii <yannik98@yandex.ru>
2021-11-07 20:56:13 +0300

    added timer

--- main.cpp
+++ main.cpp
@@ -1,10 +1,23 @@
+#include <chrono>
 #include <iostream>
 #include "src/triangle.h"
 
 using namespace std;
+using namespace std::chrono;
 
 int main() {
-    cout << triangle("tr1.txt") << endl;
-    cout << triangle_rec("tr1.txt") << endl;
+    write_random("tr1.txt", 28);
+
+
+    auto start_dp = high_resolution_clock::now();
+    cout << triangle("tr1.txt");
+    auto finish_dp = high_resolution_clock::now();
+    cout << " : " << duration_cast<milliseconds>(finish_dp - start_dp).count() << endl;
+
+
+    auto start_rec = high_resolution_clock::now();
+    cout << triangle_rec("tr1.txt");
+    auto finish_rec = high_resolution_clock::now();
+    cout << " : " << duration_cast<milliseconds>(finish_rec - start_rec).count() << endl;
     return 0;
 }
